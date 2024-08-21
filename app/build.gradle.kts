4afe536f2c8b9653f8263331cc677fbd1e915a9d
Linking Lin <Linking2Lin@Outlook.com>
2024-08-21 16:32:28 +0800

    feat: Initial setup for Appwidget module
    This commit sets up the initial structure for the Appwidget module, including:
    
    - Creation of the Appwidget module
     with basic project files.
    - Addition of necessary dependencies for Glance and Material3.
    - Implementation of a basic GlanceAppWidgetReceiver and GlanceAppWidget.
    - Declaration of the AppWidget in the main app's AndroidManifest.xml.
    - Inclusion of the Appwidget module as a dependency in the main
     app.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(project(":Appwidget"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -56,6 +57,4 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-    implementation(libs.glance)
-    implementation(libs.glance.material3)
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,3 +21,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "MyChineseCalender"
 include(":app")
+include(":Appwidget")
